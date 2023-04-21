0594f00dce74e9de3ce7b56b36666066e7cc959e
Aaron <iaaron.xyz@outlook.com>
2023-04-20 19:51:59 -0600

    fetch and render data properly

---
+++ weather.js
@@ -0,0 +1,33 @@
+function displayResults(data) {
+  const weatherInfo = document.getElementById('weather-info');
+  const weather = data.weather[0].description;
+  const temp = data.main.temp;
+  const humidity = data.main.humidity;
+  
+  weatherInfo.textContent = 'Weather: ' + weather + ', ' + 'Temperature:' + temp + ', ' + 'Humidity: ' + humidity;
+}
+
+function getWeather(city) {
+  const apiKey = "81383e7bd599e45d7534726f6e06fab2";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  // Fetch and process results
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("No se pudo obtener el clima");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      displayResults(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // Show messages while fetching
+  console.log("waiting results");
+}
